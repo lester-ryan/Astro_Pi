1e3447557524700eed549cbb86f25d7751f7c89c
Lester Ryan <79061639+lester-ryan@users.noreply.github.com>
2022-01-31 09:44:34 +0000

    Delete RP 28 Jan directory

--- RP 28 Jan/Location and file test.py
+++
@@ -1,9 +0,0 @@
-from orbit import ISS
-
-
-for i in range (100):
-    file = (open('test of file2022.txt', 'a'))
-    location = ISS.coordinates()
-    file.writelines(f'{location} {i:03d}')
-    file.writelines('\n')
-    file.close()
